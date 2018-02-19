6fc90926cea0dd6d20ba8923681e7ab0a3d65d0d
Alex Labonne <alex.labonne@ekino.com>
2018-02-19 14:02:46 +0100

    display gifs. possibility to search and manage url

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-let formElement = document.querySelector("#form");
 const giphyUrl = "http://api.giphy.com/v1/gifs/search?q";
-const apikey = "";
+const apikey = "vw4usscjAkDQiPbUvnGdRJfpEUWqZsuY";
 
+let formElement = document.querySelector("#form");
+let gifDisplay = document.querySelector(".results");
+let urlParams = new URLSearchParams(window.location.search);
+
+//Loading
+window.onload = e => {
+  let urlParam = urlParams.get('q')
+  if (urlParam) {
+    searching(urlParam, e);
+  }
+};
+
+//search
 formElement.addEventListener("submit", e => {
-  console.log(e.target[0].value);
   let inputText = e.target[0].value;
-  let url = `${giphyUrl}=${inputText}&api_key=${apikey}`;
+  searching(inputText, e);
+  urlParams == undefined ? urlParams.set('q', inputText) : window.location.href += `?q=${inputText}`;
+});
 
-  let datas = fetch(url)
+searching = (searchedText, e) => {
+  let url = `${giphyUrl}=${searchedText}&api_key=${apikey}`;
+  fetch(url)
     .then(response => {
-      response.json().then(data => console.log(data));
+      response.json().then(data => {
+        while (gifDisplay.firstChild) {
+          gifDisplay.removeChild(gifDisplay.firstChild);
+        }
+        for (let i = 0; i < data.data.length; i++) {
+          let image = document.createElement('img')
+          image.setAttribute('src', data.data[i].images.fixed_width.url);
+          image.setAttribute('alt', data.data[i].slug);
+          gifDisplay.appendChild(image);
+        }
+      });
     })
     .catch(error => console.log(error));
-
-  console.log(datas);
-
   e.preventDefault();
-});
+};
